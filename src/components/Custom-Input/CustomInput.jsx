48516910ded93210eadccf4e573fa51c201e0f9f
JoaoGabrielVianna <joaogabrielvianna05@gmail.com>
2023-11-23 22:05:57 -0300

    first commit

--- src/components/Custom-Input/CustomInput.jsx
+++ src/components/Custom-Input/CustomInput.jsx
@@ -1,11 +1,11 @@
 import './CustomInput.css'
 import customInput from '../../assets/svgs/input.svg'
-export default function CustomInput({ icon, placehover= 'Digite algo...', onChange }) {
+export default function CustomInput({ icon, type = 'text', placehover= 'Digite algo...', onChange }) {
     return (
         <>
             <main id="main-CustomInput" >
                 <div className='icon'></div>
-                <input type="text" placeholder={placehover} onChange={onChange} />
+                <input type={type} placeholder={placehover} onChange={onChange} />
                 {/* <div className='icon2'></div> */}
             </main>
         </>

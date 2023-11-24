575226ba51d377c837f54592f2a313f3a6d58da2
JoaoGabrielVianna <joaogabrielvianna05@gmail.com>
2023-11-23 22:40:51 -0300

    first commit

--- src/routes/Login-screen/Login.jsx
+++ src/routes/Login-screen/Login.jsx
@@ -1,82 +1,88 @@
 import { useState } from 'react';
-import { CustomButtonPink } from '../../components/Custom-Button/CustomButton'
-import CustomInput from '../../components/Custom-Input/CustomInput'
-import GoBackButton from '../../components/GoBack-Button/GoBackButton'
-import './Login.css'
+import { CustomButtonPink } from '../../components/Custom-Button/CustomButton';
+import CustomInput from '../../components/Custom-Input/CustomInput';
+import GoBackButton from '../../components/GoBack-Button/GoBackButton';
+import './Login.css';
 import { useNavigate } from 'react-router-dom';
 import { useUserAuth } from '../../context/userAuthContext';
 import CustomAlert from '../../components/Custom-Alert/CustomAlert';
 
 export default function LoginPage() {
-    const [buttonOpacity, setButtonOpacity] = useState(true);
-    const [error, setError] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+  // Estados para controlar o botão, mensagens de erro e dados de entrada
+  const [buttonOpacity, setButtonOpacity] = useState(true);
+  const [error, setError] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  // Expressão regular para validar e-mails
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-    const navigate = useNavigate()
-    const { logIn, signed, googleSignIn } = useUserAuth();
+  // Navegação e funções do contexto de autenticação
+  const navigate = useNavigate();
+  const { logIn, signed, googleSignIn } = useUserAuth();
 
-    const loginButton = async (e) => {
-        // Impede o envio do formulário se o botão estiver desativado
-        if (!buttonOpacity) {
-            e.preventDefault();
-            setError('');
-            try {
-                await logIn(email, password);
-                navigate('/home')
-            } catch (err) {
-                setError(err.message);
-            }
-        }
+  // Função para lidar com o clique no botão de login
+  const loginButton = async (e) => {
+    // Impede o envio do formulário se o botão estiver desativado
+    if (!buttonOpacity) {
+      e.preventDefault();
+      setError('');
+      try {
+        await logIn(email, password);
+        navigate('/home');
+      } catch (err) {
+        setError(err.message);
+      }
     }
+  };
 
-    const GoogleButton = async (e) => {
-        e.preventDefault();
-        try {
-            await googleSignIn()
-            navigate('/home')
-        } catch (err) {
-            setError(err.message);
-        }
+  // Função para lidar com o clique no botão do Google
+  const GoogleButton = async (e) => {
+    e.preventDefault();
+    try {
+      await googleSignIn();
+      navigate('/home');
+    } catch (err) {
+      setError(err.message);
     }
+  };
 
-    const InputPassword = (e) => {
-        const inputValue = e.target.value;
-        setPassword(inputValue);
-        // Lógica para verificar as condições e ajustar a opacidade
-        setButtonOpacity(inputValue.length < 6 || !emailRegex.test(email));
-    };
+  // Função para lidar com a entrada de senha
+  const InputPassword = (e) => {
+    const inputValue = e.target.value;
+    setPassword(inputValue);
+    // Lógica para verificar as condições e ajustar a opacidade
+    setButtonOpacity(!emailRegex.test(inputValue) + (inputValue.length < 6));
+  };
 
-    const InputEmail = (e) => {
-        const inputValue = e.target.value;
-        setEmail(inputValue);
-        // Lógica para verificar as condições e ajustar a opacidade
+  // Função para lidar com a entrada de e-mail
+  const InputEmail = (e) => {
+    const inputValue = e.target.value;
+    setEmail(inputValue);
+    // Lógica para verificar as condições e ajustar a opacidade
+    setButtonOpacity(!emailRegex.test(inputValue) + (inputValue.length < 6));
+  };
 
-        setButtonOpacity(inputValue.length < 6 || !emailRegex.test(inputValue));
-    };
-
-
-    if (!signed) {
-        return (
-            <>
-
-                <main id="main-login">
-                    {error && <CustomAlert text={error} />}
-                    <GoBackButton local={'/'} />
-                    <span>
-                        <CustomInput placehover='Digite o seu e-mail...' onChange={InputEmail} />
-                        <CustomInput placehover='Digite a sua senha...' onChange={InputPassword} />
-                        <CustomButtonPink text={'Google'} link={'#'} ativo onClick={GoogleButton} />
-                    </span>
-                    <span>
-                        <CustomButtonPink text={'Login'} link={'#'} ativo desativado={buttonOpacity} onClick={loginButton} />
-                    </span>
-                </main>
-            </>
-        )
-    } else {
-        navigate('/home')
-    }
+  // Renderização condicional com base no status de autenticação
+  if (!signed) {
+    return (
+      <>
+        <main id="main-login">
+          {error && <CustomAlert text={error} />}
+          <GoBackButton local={'/'} />
+          <span>
+            <CustomInput placehover='Digite o seu e-mail...' onChange={InputEmail} />
+            <CustomInput placehover='Digite a sua senha...' onChange={InputPassword} type='password' />
+            <CustomButtonPink text={'Google'} link={'#'} ativo onClick={GoogleButton} />
+          </span>
+          <span>
+            <CustomButtonPink text={'Login'} link={'#'} ativo desativado={buttonOpacity} onClick={loginButton} />
+          </span>
+        </main>
+      </>
+    );
+  } else {
+    // Redirecionar para a página home se já estiver autenticado
+    navigate('/home');
+  }
 }

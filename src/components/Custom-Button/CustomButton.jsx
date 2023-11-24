0234ed6e4d4098847ab1581a0609bef62f77f654
JoaoGabrielVianna <joaogabrielvianna05@gmail.com>
2023-11-23 21:40:28 -0300

    first commit

--- src/components/Custom-Button/CustomButton.jsx
+++ src/components/Custom-Button/CustomButton.jsx
@@ -1,13 +1,10 @@
 import './CustomButton.css'
 import { Link } from 'react-router-dom'
-import button_pink from '../../assets/svgs/button-ativo.svg'
-import button_white from '../../assets/svgs/button-desativado.svg'
-import button_email from '../../assets/svgs/button-email.svg'
 
 export function CustomButtonPink({ desativado, text = 'Teste', link, onClick }) {
     return (
         <main onClick={onClick}>
-            <Link to={link} className='main-custom' style={{ backgroundImage: `url(${button_pink})`, opacity: desativado ? 0.5 : '' }}>
+            <Link to={link} className='main-custom-pink' style={{ opacity: desativado ? 0.5 : '' }}>
                 <h1 style={{ fontSize: 16 }}>{text}</h1>
             </Link>
         </main>
@@ -18,7 +15,7 @@ export function CustomButtonPink({ desativado, text = 'Teste', link, onClick }) 
 export function CustomButtonWhite({ desativado, text = 'Teste', link, onClick }) {
     return (
         <main onClick={onClick}>
-            <Link to={link} className='main-custom' style={{ backgroundImage: `url(${button_white})`, opacity: desativado ? 0.5 : '' }}>
+            <Link to={link} className='main-custom-white' style={{ opacity: desativado ? 0.5 : '' }}>
                 <h1 style={{ fontSize: 16, color: '#363636' }}>{text}</h1>
             </Link>
         </main>
@@ -29,7 +26,7 @@ export function CustomButtonWhite({ desativado, text = 'Teste', link, onClick })
 export function CustomButtonEmail({ link, onClick }) {
     return (
         <main onClick={onClick}>
-            <Link to={link} className='main-custom' style={{ backgroundImage: `url(${button_email})` }}>
+            <Link to={link} className='main-custom-email'>
             </Link>
         </main>
     )
